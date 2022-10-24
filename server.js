8091f94bd158d89a0447a8e9e12f83d5a17992c3
Levonte Larry <you@example.com>
2022-10-24 16:56:00 -0400

    portfolio done

--- server.js
+++ server.js
@@ -6,32 +6,14 @@ const budget = require('./models/budget');
 const port = 3000
 // GET LISTEN
 app.get('/', (req, res) => {
-res.send('Hello Adult Life!');
+res.render('index.ejs');
 });
 //MIDDLEWARE
 app.use(express.urlencoded({extended: false}))
 app.use(express.static('public'))
 //REDIRECT
 app.get('/', (req, res) => res.redirect('budget'));
-//HOME/INDEX
-app.get('/budget', (req, res) => {
-res.render('index.ejs', {budget: budget}
-    );
-});
-// NEW ROUTE
-app.get('/budget/new', (req, res) => {
-    res.render('new.ejs');
-    });
-//POST/CREATE ROUTE
-app.post('/budget', (req, res) => {
-    
-    budget.push(req.body);    
-    res.redirect('/budget')   
-});
-//SHOW 
-app.get('/budget/:id', (req, res) => {
-    res.render('show.ejs', ({budget: budget[req.params.id]}));  
-});
+
 //LISTEN
 app.listen(port, () =>{
 console.log(`welcome To Mr. Budget ${port}`);
